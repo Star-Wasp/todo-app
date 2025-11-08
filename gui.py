dc0c04ebb320cd0484d931b23ef4c41419f90b42
Star_Wasp7272 <star.wasp7272@gmail.com>
2025-11-08 21:09:35 +0100

    adding while loop and implementing the add button

--- gui.py
+++ gui.py
@@ -1,14 +1,37 @@
 import functions
 import FreeSimpleGUI as sg
 
+# Constants
+FONT = ('Input Mono', 16)
+
 # Row one
 label = sg.Text("Type a to-do")
 # Row two
-input_box = sg.InputText(tooltip="Type a to-do")
+input_box = sg.InputText(tooltip="Type a to-do:", key="to-do", font=FONT)
 add_button = sg.Button("Add")
 
 # Making a window
-window = sg.Window("__My to-do app__", layout=[[label], [input_box, add_button]])
+window = sg.Window(title="__My to-do app__",
+                   layout=[[label], [input_box, add_button]],
+                   font=FONT)
 
-window.read()
+while True:
+    event, values = window.read()
+    # print(event)
+    # print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values["to-do"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case "Edit":
+            pass
+        case "Complete":
+            pass
+        case "Show":
+            pass
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
