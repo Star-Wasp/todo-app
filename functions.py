c375785ba94ea5e11723dbe430e6b3e7d51c281c
Star_Wasp7272 <star.wasp7272@gmail.com>
2025-11-08 18:07:43 +0100

    adding FILEPATH constant variable

--- functions.py
+++ functions.py
@@ -1,10 +1,12 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Takes filepath and opens file and returns list of todos """
     with open(filepath, "r") as file:
         local_todos = file.readlines()
     return local_todos
 
-def write_todos(lst, filepath="todos.txt"):
+def write_todos(lst, filepath=FILEPATH):
     """ Takes list and filepath and writes list to file """
     with open(filepath, "w") as file:
        file.writelines(lst)
